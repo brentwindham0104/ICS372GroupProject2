856dbc1e760df6105db55d592587cbefb5b7b47d
pcwer <david.xuong.tran@gmail.com>
2019-11-14 18:55:43 -0600

    Added transition stubs for each state
    Each state is moved into a package and also extends the RefrigeratorState super class.

---
+++ src/states/PowerOffDoorOpenCoolingOff.java
@@ -0,0 +1,10 @@
+package states;
+
+import events.DoorOpenEvent;
+
+public class PowerOffDoorOpenCoolingOff extends RefrigeratorState {
+    @Override
+    public void handleEvent(DoorOpenEvent event) {
+
+    }
+}
