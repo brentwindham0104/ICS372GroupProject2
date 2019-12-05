a343323260d49ab2169c5a8508a3e06d44ff2e4b
Bereket <Bereket>
2019-12-05 16:25:44 -0600

    added a starter package

--- src/buttons/DoorCloseButton.java
+++ src/buttons/DoorCloseButton.java
@@ -4,7 +4,7 @@ import events.DoorCloseEvent;
 import javafx.event.ActionEvent;
 import states.RefrigeratorContext;
 
-public class DoorCloseButton extends GuiButton{
+public class DoorCloseButton extends GuiButton {
     public DoorCloseButton(String name) {
         super(name);
     }
